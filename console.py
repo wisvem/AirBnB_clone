c8ff0856ac885d7cb3d8cfed98ebc647ded81325
el-dani-cortes <danicor83@gmail.com>
2021-02-11 21:05:32 +0000

    Added function create and Show

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 Command processor module
 """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,6 +23,26 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         'Method to should not execute anything'
         pass
+
+    def do_create(self, arg):
+        'Method to create a instance BaseModel and save it in JSON file'
+        if bool(arg) is False:
+            print("** class name missing **")
+        elif arg != 'BaseModel':
+            print("** class doesn't exist **")
+        else:
+            my_model = eval(arg)()
+            my_model.save()
+            print("{}".format(my_model.id))
+
+    def show(self, arg):
+        'Prints string representation of an instance by class name and id'
+        all_objs = storage.all()
+        for key, value in all_objs.iems():
+            if value["id"] == arg:
+                obj = all_objs[key]
+                print(obj)
+                break
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
