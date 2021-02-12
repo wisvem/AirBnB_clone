1c349ae3cd9b560df19e872c11a03c6fea18602a
Wiston Venera Macías <wisvem@hotmail.com>
2021-02-12 02:38:08 -0500

    - Created do_update - Changed all_obj to class variable for usage in every function - Changed the evaluation order in do_update (pending to do the same with the previous functions - Added a story about the last changes

--- console.py
+++ console.py
@@ -5,6 +5,8 @@ Command processor module
 import cmd
 from models.base_model import BaseModel
 from models import storage
+# import argparse
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -13,6 +15,13 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = '(hbnb) '
     listclass = ['BaseModel']
+    # Inicializé all_obj acá arriba para poder cambiar el orden en que se
+    # evaluan las condiciones del update
+    # También cambié donde teniasmos all_obj por self.all_obj
+    # E códi pasa pep8, y ahora mismo son las 2.30 y abajo vas a encontrar una
+    # historia que la escribi a las 2
+    all_objs = storage.all()
+    # parser = argparse.ArgumentParser()
 
     def do_EOF(self, arg):
         'EOF method to exit cmd program\n'
@@ -39,19 +48,18 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         'Prints string representation of an instance by class name and id\n'
-        listclass = ['BaseModel']
-        mylist = str.split(arg)
+        mylist = shlex.split(arg)
         found_id = False
         if bool(arg) is False:
             print("** class name missing **")
-        elif mylist[0] not in listclass:
+        elif mylist[0] not in self.listclass:
             print("** class doesn't exist **")
         elif len(mylist) != 2:
             print("** instance id missing **")
         else:
             search = mylist[0]+'.'+mylist[1]
-            all_objs = storage.all()
-            for key, value in all_objs.items():
+            # all_objs = storage.all()
+            for key, value in self.all_objs.items():
                 if key == search:
                     print(value)
                     found_id = True
@@ -61,7 +69,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, arg):
         'Deletes an instance based on the class name and id\n'
-        mylist = str.split(arg)
+        mylist = shlex.split(arg)
         found_id = False
         if bool(arg) is False:
             print("** class name missing **")
@@ -71,10 +79,10 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
         else:
             search = mylist[0]+'.'+mylist[1]
-            all_objs = storage.all()
-            for key, value in all_objs.items():
+            # all_objs = storage.all()
+            for key, value in self.all_objs.items():
                 if key == search:
-                    del all_objs[key]
+                    del self.all_objs[key]
                     storage.save()
                     found_id = True
                     break
@@ -83,18 +91,20 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         'Prints all string repr of all instances based or not on class name\n'
-        all_objs = storage.all()
+        # all_objs = storage.all()
         mylist = []
         found_class = False
         if bool(arg) is False:
-            for key, value in all_objs.items():
-                obj = all_objs[key]
+            for key, value in self.all_objs.items():
+                obj = self.all_objs[key]
                 mylist.append(str(obj))
-            print(mylist)
+                found_class = True
+            if found_class is True:
+                print(mylist)
         else:
             for key, value in all_objs.items():
                 if arg == value.__class__.__name__:
-                    obj = all_objs[key]
+                    obj = self.all_objs[key]
                     mylist.append(str(obj))
                     found_class = True
             if found_class:
@@ -104,25 +114,57 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, arg):
         'Updates an obj based on the class name and id by adding or updating\n'
-        mylist = str.split(arg)
+        # mylist = str.split(arg)
+        mylist = shlex.split(arg)
+        # print(m2)
+        # exit()
+        """
+        Si pude lograrlo, son las 2AM, por si no me ves levantado tan temprano
+        Cambie todos los str.split por shlex.split para que no lo hiciera por
+        los espacios sino por los argumentos
+        y pudiera tener en cuenta las comillas dobles-----
+
+        Dani est de aqui está comentado porque los argumentos no están entrando
+        correctamente.
+        el metodo ya me actualiza el objeto pero no acepta ls comillas dobles.
+        Cuando se usan comillas dobles para el valor del nuevo atributo se
+        actualiza el objeto con varios backslash
+        el metodo split separa los argumentos por espacio, si se envia un
+        "Daniel Cortes" el split devulce "Daniel, Cortes"
+
+        """
+        myobj = None
         found_id = False
         if bool(arg) is False:
             print("** class name missing **")
+            return
         elif mylist[0] not in self.listclass:
             print("** class doesn't exist **")
-        elif len(mylist) != 2:
+            return
+        elif len(mylist) < 2:
             print("** instance id missing **")
+            return
         else:
             search = mylist[0]+'.'+mylist[1]
-            all_objs = storage.all()
-            for key, value in all_objs.items():
+            for key, value in self.all_objs.items():
                 if key == search:
-                    del all_objs[key]
-                    storage.save()
+                    myobj = self.all_objs[key]
+                #    setattr(myobj, mylist[2], mylist[3])
+                #    myobj.save()
                     found_id = True
                     break
             if found_id is False:
-                print("** no instance found **")     
+                print("** no instance found **")
+                return
+        if len(mylist) < 3:
+            print("attribute name missing")
+        elif len(mylist) < 4:
+            print("** value missing **")
+        else:
+            setattr(myobj, mylist[2], mylist[3])
+            myobj.save()
+            found_id = True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -7,10 +7,12 @@ print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
     print(obj)
-
+arg =["name", "Juancho"]
 print("-- Create a new object --")
 my_model = BaseModel()
 my_model.name = "Holberton"
 my_model.my_number = 89
+print("==================\n",type(my_model))
+setattr(my_model, arg[0], arg[1])
 my_model.save()
 print(my_model)
