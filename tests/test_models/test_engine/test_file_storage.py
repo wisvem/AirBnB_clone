033cad9a0ef5d2330b42739809f9b94040bc23cd
el-dani-cortes <danicor83@gmail.com>
2021-02-16 20:29:37 -0500

    Added more and more and more test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -246,13 +246,22 @@ class Test_engine(unittest.TestCase):
         self.assertNotEqual(getattr(FileStorage, "_FileStorage__objects"), [])
         self.assertEqual(FileStorage._FileStorage__file_path, "file.json")
 
-    def test_engine_016(self):
-        """Test if all_obs is empty"""
-        fs = FileStorage()
-        path = "file.json"
-        self.assertIsInstance(fs, FileStorage)
-        fs.save()
-        self.assertTrue(os.path.exists(path))
+
+my_dict = {'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337',
+           'created_at': '2017-09-28T21:03:54.052298',
+           '__class__': 'BaseModel', 'my_number': 89,
+           'updated_at': '2017-09-28T21:03:54.052302',
+           'name': 'Holberton'}
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    Unittest for file_storage.py
+    """
+    storage = FileStorage()
+    path = storage._FileStorage__file_path
+    bm_instance = BaseModel(**my_dict)
+    storage.new(bm_instance)
 
     def test_methods_docstring(self):
         """
@@ -260,78 +269,32 @@ class Test_engine(unittest.TestCase):
         """
         methods = inspect.getmembers(FileStorage, predicate=inspect.ismethod)
         for name, func in methods:
-            self.assertTrue(len(func.__doc__) > 5)
+            self.assertTrue(len(func.__doc__) > 10)
         methods = inspect.getmembers(FileStorage, predicate=inspect.isfunction)
         for name, func in methods:
-            self.assertTrue(len(func.__doc__) > 5)
+            self.assertTrue(len(func.__doc__) > 10)
+
+    def test_storage_isinstance(self):
+        """
+        Tests if storage is an instance of FileStorage
+        """
+        self.assertIsInstance(TestFileStorage.storage, FileStorage)
+
+    def test_file_json(self):
+        """
+        Tests for path existence
+        """
+        TestFileStorage.storage.save()
+        self.assertTrue(os.path.exists(TestFileStorage.path))
 
     def test_save_another_instance(self):
         """
         Tests for save another instance in path
         """
-        path = "file.json"
-        fs = FileStorage()
         bm2_instance = BaseModel()
         bm2_instance.save()
         key = type(bm2_instance).__name__ + "." + str(bm2_instance.id)
-        with open(path, mode="r", encoding="utf-8") as f:
+        with open(TestFileStorage.path, mode="r", encoding="utf-8") as f:
             reader = json.load(f)
         self.assertEqual(
-            reader[key], fs.all()[key].to_dict())
-
-
-# my_dict = {'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337',
-#            'created_at': '2017-09-28T21:03:54.052298',
-#            '__class__': 'BaseModel', 'my_number': 89,
-#            'updated_at': '2017-09-28T21:03:54.052302',
-#            'name': 'Holberton'}
-
-
-# class TestFileStorage(unittest.TestCase):
-#     """
-#     Unittest for file_storage.py
-#     """
-#     storage = FileStorage()
-#     path = storage._FileStorage__file_path
-#     bm_instance = BaseModel(**my_dict)
-#     storage.new(bm_instance)
-
-#     # def test_methods_docstring(self):
-#     #     """
-#     #     Tests docstring for methods
-#     #     """
-#     #     methods = inspect.getmembers(FileStorage, predicate=inspect.ismethod)
-#     #     for name, func in methods:
-#     #         self.assertTrue(len(func.__doc__) > 10)
-#     #     methods = inspect.getmembers(FileStorage, predicate=inspect.isfunction)
-#     #     for name, func in methods:
-#     #         self.assertTrue(len(func.__doc__) > 10)
-
-#     def test_storage_isinstance(self):
-#         """
-#         Tests if storage is an instance of FileStorage
-#         """
-#         self.assertIsInstance(TestFileStorage.storage, FileStorage)
-
-#     def test_file_json(self):
-#         """
-#         Tests for path existence
-#         """
-#         TestFileStorage.storage.save()
-#         self.assertTrue(os.path.exists(TestFileStorage.path))
-
-#     def test_save_another_instance(self):
-#         """
-#         Tests for save another instance in path
-#         """
-#         bm2_instance = BaseModel()
-#         bm2_instance.save()
-#         key = type(bm2_instance).__name__ + "." + str(bm2_instance.id)
-#         with open(TestFileStorage.path, mode="r", encoding="utf-8") as f:
-#             reader = json.load(f)
-#         self.assertEqual(
-#             reader[key], TestFileStorage.storage.all()[key].to_dict())
-
-
-# if __name__ == '__main__':
-#     pass
+            reader[key], TestFileStorage.storage.all()[key].to_dict())
