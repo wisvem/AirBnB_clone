01dd7ae819641f6ff1481a580f44b666a23cf3fe
Wiston Venera Macías <wisvem@hotmail.com>
2021-02-17 19:17:20 -0500

    SEXTO

--- tests/test_console.py
+++ tests/test_console.py
@@ -147,10 +147,6 @@ class Test_console(unittest.TestCase):
             st = f.getvalue()
             self.assertEqual(msg, st)
 
-
-class TestHBNBCommand_prompting(unittest.TestCase):
-    """Unittests for testing prompting of the HBNB command interpreter."""
-
     def test_prompt_string(self):
         self.assertEqual("(hbnb) ", HBNBCommand.prompt)
 
